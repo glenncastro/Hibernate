b0d4f39c1a848b34911745f2dfe6d4e1e92d4f4e
Glenn Castro <glenn.castro@gmail.com>
2015-01-03 16:19:32 +1100

    Unit3 - Version 6 - mappedBy (OneToMany / ManyToOne)

---
+++ Unit3/FirstHibernateProj/src/main/java/v6/com/castro/hibernate/HibernateTest.java
@@ -0,0 +1,43 @@
+package com.castro.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import com.castro.dto.UserDetails;
+import com.castro.dto.Vehicle;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		UserDetails user = new UserDetails();
+		user.setUserName("First User");	
+		
+		Vehicle vehicle = new Vehicle();
+		vehicle.setVehicleName("Car");
+		
+		Vehicle vehicle2 = new Vehicle();
+		vehicle2.setVehicleName("Jeep");
+		
+		user.getVehicle().add(vehicle);
+		user.getVehicle().add(vehicle2);
+		
+		vehicle.setUser(user);
+		vehicle2.setUser(user);
+		
+		Configuration configuration = new Configuration().configure();
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+			.applySettings(configuration.getProperties());
+		SessionFactory factory = configuration.buildSessionFactory(builder.build());
+
+		Session session = factory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.save(vehicle);
+		session.save(vehicle2);
+		session.getTransaction().commit();
+		session.close();
+		
+	}
+}
