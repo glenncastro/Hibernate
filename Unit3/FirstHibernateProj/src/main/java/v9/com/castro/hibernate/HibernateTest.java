0fc3dddafb6ebcb42081986c892b99afa8093109
Glenn Castro <glenn.castro@gmail.com>
2015-01-04 16:21:11 +1100

    Unit3 - Version 9 - Implementing Inheritance

---
+++ Unit3/FirstHibernateProj/src/main/java/v9/com/castro/hibernate/HibernateTest.java
@@ -0,0 +1,39 @@
+package com.castro.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import com.castro.dto.FourWheeler;
+import com.castro.dto.TwoWheeler;
+import com.castro.dto.Vehicle;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {		
+		Vehicle vehicle = new Vehicle();
+		vehicle.setVehicleName("Car");
+		
+		TwoWheeler bike = new TwoWheeler();
+		bike.setVehicleName("Bike");
+		bike.setSteeringHandle("Bike Steering Handle");
+		
+		FourWheeler car = new FourWheeler();
+		car.setVehicleName("Porsche");
+		car.setSteeringWheel("Porsche Steering Wheel");
+		
+		Configuration configuration = new Configuration().configure();
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+			.applySettings(configuration.getProperties());
+		SessionFactory factory = configuration.buildSessionFactory(builder.build());
+
+		Session session = factory.openSession();
+		session.beginTransaction();
+		session.save(vehicle);
+		session.save(bike);
+		session.save(car);
+		session.getTransaction().commit();
+		session.close();		
+	}
+}
